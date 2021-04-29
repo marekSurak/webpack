8d844718eb6a9c72636556c24f8b0e513df0b0cc
Marek Surak <mareksurak@strv.com>
2021-04-29 09:59:35 +0200

    add webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const { resolve } = require("path");
+const webpackValidator = require("webpack-validator");
 
 module.exports = () => {
-  return {
+  return webpackValidator({
     context: resolve("src"),
     entry: "./bootstrap.js",
     output: {
       filename: "bundle.js",
     },
-  };
+  });
 };
